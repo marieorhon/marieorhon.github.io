bf491a4bcf11acc9759649a3259268f6503828a0
paul604 <paul604@users.noreply.github.com>
2021-03-28 18:45:38 +0200

    add IE support XD

---
+++ js/script.js
@@ -0,0 +1,8 @@
+if (navigator.userAgent.indexOf('MSIE')>0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+    document.write('\
+    <div class="alert alert-warning" role="alert">\
+    Désolé mais ce site ne supporte pas Internet Explorer. </br>\
+    Merci d\'utiliser  Microsoft Edge, Google Chrome ou Firefox.\
+    </div>\
+    ');
+};
